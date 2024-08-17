adecbfaf5aeec700552fc70b5acdec7809a39ade
Samuel Erdö <samuelerdo@gmx.at>
2024-08-17 12:54:35 +0200

    implemented smoother player movement

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -3,18 +3,44 @@ using Godot;
 public partial class PlayerMovement : CharacterBody2D
 {
     [Export]
-    public int Speed { get; set; } = 400;
+    public int Speed { get; set; } = 800;
 
-    public void GetInput()
+    [Export]
+    public float Inertia { get; set; } = 1000f;
+
+    [Export]
+    public float RotationSpeed { get; set; } = 50f;
+
+    private Vector2 _velocity = Vector2.Zero;
+    private const float RotationTreshold = 50f;
+
+    public void GetInput(float delta)
     {
-
         Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
-        Velocity = inputDirection * Speed;
+
+        if (inputDirection != Vector2.Zero)
+        {
+            _velocity = _velocity.MoveToward(inputDirection * Speed, Inertia * delta);
+        }
+        else
+        {
+            _velocity = _velocity.MoveToward(Vector2.Zero, Inertia * delta);
+        }
+
+        // apply Velocity
+        Velocity = _velocity;
+
+        // Rotate ship in towards moving direction
+        if (_velocity.Length() > RotationTreshold)
+        {
+            float targetRotation = _velocity.Angle();
+            Rotation = Mathf.LerpAngle(Rotation, targetRotation, RotationSpeed * (float)delta);
+        }
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        GetInput();
+        GetInput((float)delta);
         MoveAndSlide();
     }
 }

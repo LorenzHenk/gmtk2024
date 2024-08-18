24f684b0d283a99de151017339195dbbd53c9c97
Samuel Erdö <samuelerdo@gmx.at>
2024-08-18 15:31:04 +0200

    added randomized asteroids

--- Asteroid.cs
+++ Asteroid.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 public partial class Asteroid : Node2D
@@ -5,20 +6,24 @@ public partial class Asteroid : Node2D
 	[Signal]
     public delegate void PlayerCollisionEventHandler();
 
-	[Export]
-	public double RotationSpeed = 0.25;
+	private double RotationSpeed;
 
 	private AnimatedSprite2D _animatedSprite;
 	private Sprite2D _sprite;
 
     public override void _Ready()
     {
+		base._Ready();
+
+		RotationSpeed = GD.RandRange(-0.9, 0.9);
+		Scale = Vector2.One * (float)GD.RandRange(0.75, 2.75);
 		PlayerCollision += OnPlayerCollision;
     }
 
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
+		
 		Rotate((float)(RotationSpeed * delta));
     }
 

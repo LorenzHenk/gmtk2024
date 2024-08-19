ade4d5718742429bbc3905d676a860a748041643
Samuel Erdö <samuelerdo@gmx.at>
2024-08-19 22:55:16 +0200

    Added loop to background music

--- Space.cs
+++ Space.cs
@@ -3,9 +3,14 @@ using System.Threading.Tasks;
 
 public partial class Space : Node2D
 {
+	private AudioStreamPlayer theme;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+
+		theme = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+		theme.Play();
 
 		// attach scene-handler to liftoff button
 		var sceneHandler = GetNodeOrNull<SceneHandler>("/root/SceneHandler");
@@ -30,6 +35,10 @@ public partial class Space : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if(!theme.Playing)
+		{
+			theme.Play();
+		}
 	}
 
 	public void BackToBase()
